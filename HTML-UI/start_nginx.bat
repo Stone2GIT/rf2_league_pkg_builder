756fcaa6fb40a2f6409998d891cf773fb5bc7321
Stone <info@simracingjustfair.org>
2024-07-18 21:52:38 +0200

    simple batch file for starting nginx

---
+++ HTML-UI/start_nginx.bat
@@ -0,0 +1,8 @@
+taskkill /fi "imagename eq nginx.exe" /F
+taskkill /fi "imagename eq php-cgi.exe" /F
+cd nginx
+start nginx
+tasklist /fi "imagename eq nginx.exe"
+cd ..\php
+php-cgi.exe -b 127.0.0.1:9123
+
